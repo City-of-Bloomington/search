22b5857f21ed4d5d00df44bd223ae16884f946ad
inghamn <inghamn@bloomington.in.gov>
2021-09-20 10:29:36 -0400

    Facet on Board names
    Updates #3

--- src/Web/Search/Solr.php
+++ src/Web/Search/Solr.php
@@ -13,8 +13,9 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Solr
 {
-    public const DEFAULT_FIELD = 'tm_X3b_en_aggregated_field';
-    public static $FACETS      = ['index_id', 'ss_type'];
+    public const  DEFAULT_FIELD = 'tm_X3b_en_aggregated_field';
+    public static $FACETS       = ['index_id', 'ss_type', 'ss_board'];
+
 
     private $client;
 
@@ -50,14 +51,29 @@ class Solr
                           ?array $filters=null): ResultInterface
     {
         $search = self::cleanInput($search);
-        $query  = $this->client->createSelect([
-            'query'   => $search,
-            'fields'  => 'id,site,index_id,ss_type,ss_url,ss_title,ss_summary,score',
-            'start'   => $currentPage - 1, // Solr page numbers start at 0
-            'rows'    => $itemsPerPage,
-            'querydefaultfield' => self::DEFAULT_FIELD
-        ]);
+
+        $query  = $this->client->createSelect();
+        $dismax = $query->getEDisMax();
+
         $query->getHighlighting();
+
+        $query->setQuery ($search);
+        $query->setFields('id,site,index_id,ss_type,ss_board,ss_url,ss_title,ss_summary,score');
+        $query->setStart ($currentPage - 1); // Solr pagination starts at 0
+        $query->setRows  ($itemsPerPage);
+
+        $dismax->setQueryFields('ss_title^2 ss_summary^2 tm_X3b_en_aggregated_field');
+
+        // This filters out old documents from search results
+        // It declares a curve from 1 to zero where it hits zero at $M.
+        // So, documents older than $m will not show up in search results at all.
+        // $a and $b control the shape of the curve.
+        // Units are all in milliseconds
+        $numYears = 1;
+        $m = 3.16E-11 * $numYears;
+        $a = 1; $b = 1;
+        $dismax->setBoostFunctionsMult("recip(ms(NOW,ds_changed),$m,$a,$b)");
+//         $dismax->setBoostFunctionsMult("if(eq(ss_type,'news'),0.1,1) recip(ms(NOW,ds_changed),1,1000,1000)");
 
         $facets = $query->getFacetSet();
         foreach (self::$FACETS as $f) {
@@ -65,11 +81,12 @@ class Solr
 
             if (!empty($filters[$f])) {
                 $query->createFilterQuery($f)
-                      ->setQuery("$f: ".self::cleanInput($filters[$f]));
+                      ->setQuery(sprintf('%s:"%s"', $f, self::cleanInput($filters[$f])));
             }
         }
 
-
+        $req = $this->client->createRequest($query);
+        error_log($req->getUri());
         return $this->client->execute($query);
     }
 
