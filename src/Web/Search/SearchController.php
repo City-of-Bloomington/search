e5faaa49f45f00762556493224505f7b5537d871
Cliff Ingham <inghamn@bloomington.in.gov>
2026-07-17 09:59:10 -0400

    Clean facet values with regex
    Updates #33

--- src/Web/Search/SearchController.php
+++ src/Web/Search/SearchController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2021-2024 City of Bloomington, Indiana
+ * @copyright 2021-2026 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -11,23 +11,40 @@ use Web\Controller;
 class SearchController extends Controller
 {
     public const ITEMS_PER_PAGE = 20;
+    public const MAX_PAGE       = 1000;
 
     public function __invoke()
     {
         // Solr pages start at 0
 		$page = !empty($_GET['page']) ? (int)$_GET['page'] - 1 : 0;
+        if ($page > self::MAX_PAGE) { $page = 0; }
 
-        $filters = [];
-        foreach (Solr::$FACETS as $f) {
-            if (!empty($_GET[$f])) { $filters[$f] = $_GET[$f]; }
-        }
+        $filters = self::filters($_GET);
 
-        $query = empty($_GET['query']) ? '*' : preg_replace('/[^a-zA-Z0-9\s]/', ' ', $_GET['query']);
+        $query = empty($_GET['query']) ? '*' : preg_replace('/[^\w\x20]/', ' ', $_GET['query']);
         $rows  = ($filters || !empty($_GET['query'])) ? self::ITEMS_PER_PAGE : 0;
 
         $solr  = $this->di->get('Web\Search\Solr');
         $res   = $solr->query($query, $rows, $page, $filters);
 
         return new SearchView($res, self::ITEMS_PER_PAGE, $page);
+    }
+
+    private static function filters(array $get): array
+    {
+        $filters = [];
+        foreach (Solr::$FACETS as $f) {
+            if (!empty($_GET[$f])) {
+                switch ($f) {
+                    case 'its_year':
+                        $filters['its_year'] = preg_replace('/[^\d]/', '', $_GET['its_year']);
+                    break;
+
+                    default:
+                        $filters[$f] = preg_replace('/[^\w\x20]/', '', $_GET[$f]);
+                }
+            }
+        }
+        return $filters;
     }
 }
